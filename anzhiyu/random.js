61d5ad2d6bc117a9833930702b4c80a5d73902f5
apieceofzhiyan <zhiyanzhiyan1057@gmail.com>
2024-03-24 22:39:45 +0800

    Site updated: 2024-03-24 22:39:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/23/双曲正弦函数/","2023/12/31/一道外国不等式/","2024/03/20/如何提高你的注意力/","2024/03/24/角动量简介/"];function toRandomPost(){
+var posts=["2024/03/24/角动量简介/","2023/12/31/一道外国不等式/","2024/03/20/如何提高你的注意力/","2024/03/23/双曲正弦函数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
